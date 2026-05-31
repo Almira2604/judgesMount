16a359210916c6a9de151a91ef36a16429fa02aa
Almira2604 <almiraofubu@gmail.com>
2026-05-31 13:17:39 +0100

    fix: adjust pastor image case sensitivity in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaCross, FaFire, FaHeart, FaBookOpen } from 'react-icons/fa'
 import { LuShieldCheck, LuSparkles } from 'react-icons/lu'
-import Pastor from '../assets/Pastor.jpg' // Adjust path based on your file setup
+import Pastor from "../assets/pastor.jpg"; 
 
 function About() {
   const coreValues = [
